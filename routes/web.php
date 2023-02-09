ef4545374c1e83b7f3b921486c51365c5df0981a
Cliff-Ezra <cliffezra212@gmail.com>
2023-02-09 10:07:05 +0300

    Cleaned up the Routes as well as added Filtering by tags

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,21 +15,17 @@ use App\Models\Listing;
 |
 */
 
+// Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing  
+
 //All Listing
-Route::get('/', function () {
-    return view('listings', [
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 // Single Listing
-Route::get('/listing/{listing}', function (Listing $listing) {
-    return view('listing', [
-        'listing' => $listing
-    ]);
-});
-
-// Testing Connection
-Route::get('/dbconn', function () {
-    return view('dbconn');
-});
+Route::get('/listing/{listing}', [ListingController::class, 'show']);
